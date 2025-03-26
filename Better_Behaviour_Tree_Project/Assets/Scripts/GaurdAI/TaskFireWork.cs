833c2e7fc42fde5f24e16213faca3aa4f55cd275
6185jcaron <66760646+Ace-03@users.noreply.github.com>
2025-03-26 10:01:21 -0700

    added fireworks

--- Better_Behaviour_Tree_Project/Assets/Scripts/GaurdAI/TaskFireWork.cs
+++ Better_Behaviour_Tree_Project/Assets/Scripts/GaurdAI/TaskFireWork.cs
@@ -5,10 +5,30 @@ using BehaviorTree;
 
 public class TaskFireWork : Node
 {
+    private GameObject _knight;
+    private GuardBT _guardBT;
+
     public override NodeState Evaluate()
     {
+        _knight = GameObject.Find("ToonRTS_demo_Knight");
+
+        _guardBT = _knight.GetComponent<GuardBT>();
+
+        Debug.Log("Firework");
+        Debug.Log(_guardBT.fireWork);
+        //if (true)
+
+        if (!_guardBT.fireWork.isPlaying)
+        {
+            _guardBT.fireWork.Play();
+            state = NodeState.RUNNING;
+            return state;
+
+        }
 
 
+
+        //_guardBT.fireWork.Stop();
         state = NodeState.FAILURE;
         return state;
     }
